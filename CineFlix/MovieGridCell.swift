e4516ea778185daa287d11703caf7f5e221d9d99
Srikar Prayaga <50150338+SrikarPrayaga06@users.noreply.github.com>
2020-02-06 17:51:58 -0800

    Add files via upload

---
+++ CineFlix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  CineFlix
+//
+//  Created by Srikar Prayaga on 1/23/20.
+//  Copyright © 2020 Srikar Prayaga. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
